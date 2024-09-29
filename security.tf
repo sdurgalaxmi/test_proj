93a4ea488030e2925246f3ce936e0a0a8f286203
sdurgalaxmi <s.durgalaxmi@gmail.com>
2024-09-29 12:35:57 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,7 +1,33 @@
-resource "aws_security_group" "allow_ssh" {
-  name        = "demo_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = "vpc-0b30226ed9787e925"
+resource "aws_security_group" "k8s_sg" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # http
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Restrict for security in production - https
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Add more ingress rules as necessary for your setup
+
+  tags = {
+    Name = "demo-k8s-sg"
+  }
+}
 
   ingress {
     description = "SSH into VPC"
