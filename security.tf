dfd9cf4d442d1ec4bbace4d9544dc61c66939d3a
sdurgalaxmi <s.durgalaxmi@gmail.com>
2024-09-29 12:37:13 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -27,28 +27,4 @@ resource "aws_security_group" "k8s_sg" {
   tags = {
     Name = "demo-k8s-sg"
   }
-}
-
-  ingress {
-    description = "SSH into VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
- ingress {
-    description = "SSH into VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    description = "Outbound Allowed"
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+} 
