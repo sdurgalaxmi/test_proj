d4b62aac449ecd07c444430181d4e23481727108
sdurgalaxmi <s.durgalaxmi@gmail.com>
2024-09-29 12:52:00 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,3 +1,20 @@
+resource "aws_vpc" "k8s_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "demo-k8s-vpc"
+  }
+}
+
+resource "aws_subnet" "k8s_subnet" {
+  vpc_id     = aws_vpc.k8s_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "demo-k8s-subnet"
+  }
+}
+
 resource "aws_security_group" "k8s_sg" {
   vpc_id = aws_vpc.k8s_vpc.id
 
