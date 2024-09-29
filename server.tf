a84349fa44cb87f6084e7eaf6dc98364c66ea209
sdurgalaxmi <s.durgalaxmi@gmail.com>
2024-09-29 20:12:30 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -35,31 +35,31 @@ resource "aws_subnet" "k8s_subnet" {
   }
 }
 
-resource "aws_internet_gateway" "k8s_igw" {
-  vpc_id = var.existing_vpc_id
+#resource "aws_internet_gateway" "k8s_igw" {
+#  vpc_id = var.existing_vpc_id
+#
+ # tags = {
+ #   Name = "demo-k8s-igw"
+ # }
+#}
 
-  tags = {
-    Name = "demo-k8s-igw"
-  }
-}
+#resource "aws_route_table" "k8s_route_table" {
+#  vpc_id = var.existing_vpc_id
+#
+ # tags = {
+ #   Name = "demo-k8s-rt"
+ # }
 
-resource "aws_route_table" "k8s_route_table" {
-  vpc_id = var.existing_vpc_id
+ # route {
+  #  cidr_block = "0.0.0.0/0"
+  #  gateway_id = aws_internet_gateway.k8s_igw.id
+ # }
+#}
 
-  tags = {
-    Name = "demo-k8s-rt"
-  }
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.k8s_igw.id
-  }
-}
-
-resource "aws_route_table_association" "k8s_route_table_assoc" {
-  subnet_id      = aws_subnet.k8s_subnet.id
-  route_table_id = aws_route_table.k8s_route_table.id
-}
+#resource "aws_route_table_association" "k8s_route_table_assoc" {
+ # subnet_id      = aws_subnet.k8s_subnet.id
+#  route_table_id = aws_route_table.k8s_route_table.id
+#}
 
 resource "aws_security_group" "k8s_sg" {
   name        = "durga_ssh"
