ef230cd96a1dedfb7c8d626fd7d70b2ed1438a42
sdurgalaxmi <s.durgalaxmi@gmail.com>
2024-09-29 13:44:15 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -130,7 +130,6 @@ output "worker_ips" {
 
 
 provisioner "local-exec" {
-    command = "echo The servers IP address is ${aws_instance.master.public_ip} && echo ${aws_instance.master.public_ip} > /root/inv"
     command = "echo The servers IP address is ${aws_instance.worker[*].public_ip} && echo ${aws_instance.worker[*].public_ip} > /root/inv"
   }
 }
