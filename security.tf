4524d7a06d5219a0b2be21c7f3fc3cea50f84ff4
sdurgalaxmi <s.durgalaxmi@gmail.com>
2024-09-29 12:20:06 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "durga_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "vpc-0b30226ed9787e925"
+
+  ingress {
+    description = "SSH into VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+ ingress {
+    description = "SSH into VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Outbound Allowed"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
