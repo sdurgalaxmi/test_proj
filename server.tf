578756c7874e059433064c5eddc7ade2b33745e5
sdurgalaxmi <s.durgalaxmi@gmail.com>
2024-09-29 12:55:38 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -12,6 +12,54 @@ region = "us-east-2"
 }
 
 
+resource "aws_vpc" "k8s_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "demo-k8s-vpc"
+  }
+}
+
+resource "aws_subnet" "k8s_subnet" {
+  vpc_id     = aws_vpc.k8s_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "demo-k8s-subnet"
+  }
+}
+
+resource "aws_security_group" "k8s_sg" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # http
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Restrict for security in production - https
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Add more ingress rules as necessary for your setup
+
+  tags = {
+    Name = "demo-k8s-sg"
+  }
+} 
+
 
 resource "aws_instance" "master" {
   ami           = "ami-085f9c64a9b75eed5"  # Change to your preferred AMI (e.g., Ubuntu)
